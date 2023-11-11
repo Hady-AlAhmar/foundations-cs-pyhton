5131ac30c62bf9f138e200745b2fe931b2b448c4
Hady Al Ahmar <hadyred@outlook.com>
2023-11-12 01:54:15 +0200

    Open Tab Functionality

--- Midterm.py
+++ Midterm.py
@@ -1,8 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 10 22:24:10 2023
 
-@author: hady
-"""
+tabs = {}
+
+
+open_tabs_order = []
+
+# Added the ability to open a new tab and check if a tab is already open.
+
+def open_tab(tab_name):
+    """Open a new tab."""
+    if tab_name not in tabs:
+        tabs[tab_name] = {}
+        open_tabs_order.append(tab_name)
+        print(f"Tab '{tab_name}' opened successfully.")
+    else:
+        print(f"Tab '{tab_name}' is already open.")
+
 
